6a8cb94b48d98b4729e6c0afb9d4100452ea32b2
ronzhina <ronzhina@skbkontur.ru>
2020-03-22 21:41:29 +0300

    1, 2 задания

--- tasks/1.tic-tac-toe/index.js
+++ tasks/1.tic-tac-toe/index.js
@@ -1,17 +1,24 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+const SIZE = 3;
 
 const container = document.getElementById('fieldWrapper');
+
+let roundInfo = {
+    round : 0,
+    winnerExist : false,
+    field : resetField(),
+};
+
 
 startGame();
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+function startGame() {
+    renderGrid(SIZE);
 }
-
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -26,41 +33,139 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
-
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function cellClickHandler(row, col) {
+    console.group('Cell Info')
+    console.log(`Position: ${row}, ${col}`);
+    console.log(`Value before click: ${roundInfo.field[row][col]}`);
+    if (roundInfo.field[row][col] != 0 || roundInfo.winnerExist) {
+        return;
+    }
+    roundInfo.round++;
+    roundInfo.field[row][col] = roundInfo.round % 2 ? 1 : -1;
+    renderSymbolInCell(roundInfo.round % 2 ? CROSS : ZERO, row, col);
+    checkWinner();
+    console.log(`Value before after: ${roundInfo.field[row][col]}`);
+    console.groupEnd();
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function checkWinner() {
+    let sum;
+
+    for (let i = 0; i < SIZE; i++) {
+
+        sum = sumCells(i, 0, 0, 1);
+        if (calculateWinner(sum)) {
+            renderColorInCell(i, 0, 0, 1);
+            return;
+        }
+        //console.log(`Sum ${sum}`);
+    }
+    for (let j = 0; j < SIZE; j++) {
+        sum = sumCells(0, j, 1, 0);
+        if (calculateWinner(sum)) {
+            renderColorInCell(0, j, 1, 0);
+            return;
+        }
+    }
+
+    sum = sumCells(0, 0, 1, 1);
+    if (calculateWinner(sum)) {
+        renderColorInCell(0, 0, 1, 1);
+        return;
+    }
+
+    sum = sumCells(0, SIZE-1, 1, -1);
+    if (calculateWinner(sum)) {
+        renderColorInCell(0, SIZE-1, 1, -1);
+        return;
+    }
+
+    if (roundInfo.round === 9) {
+        alert("Победила дружба!");
+    }
+}
+
+function sumCells(rowStart, colStart, rowStep = 1, colStep = 1) {
+    let sum = 0
+    let row = rowStart;
+    let col = colStart;
+    let iteration = 1;
+    do{
+        sum += roundInfo.field[row][col];
+        row += rowStep;
+        col += colStep;
+        iteration ++;
+    } while(iteration <= SIZE); 
+    return sum;
+}
+
+function calculateWinner(sum) {
+    if (sum === SIZE) {
+        roundInfo.winnerExist = true;
+        alert("Победил X!");
+        return true;
+    }
+    if (sum === -SIZE) {
+        roundInfo.winnerExist = true;
+        alert("Победил O!");
+        return true;
+    }
+    return false
+}
+
+function renderColorInCell(rowStart, colStart, rowStep = 1, colStep = 1, color = '#d33') {
+    let row = rowStart;
+    let col = colStart;
+    let iteration = 1;
+    do{
+        let targetCell = findCell(row, col);
+        targetCell.style.color = color;
+        row += rowStep;
+        col += colStep;
+        iteration ++;
+    } while(iteration <= SIZE); 
+}
+
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
     console.log('reset!');
+    roundInfo.field = resetField();
+    roundInfo.round = 0;
+    roundInfo.winnerExist = false;
+    startGame();
+}
+
+function resetField() {
+    let arr = [];
+    for (let i = 0; i < SIZE; i++) {
+        arr[i] = [];
+        for (let j = 0; j < SIZE; j++) {
+            arr[i][j] = 0;
+        }
+    }
+    return arr;
 }
 
 
 /* Test Function */
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -71,7 +176,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -84,6 +189,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
--- tasks/2.todo-statistic/index.js
+++ tasks/2.todo-statistic/index.js
@@ -12,14 +12,179 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    const [com, args] = command.split(' ');
+
+    switch (com) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'files':
+            console.log(files);
+            break;
+        case 'show':
+            printTodos(getTodoComments());
+            break;
+        case 'important':
+            printTodos(getImportantTodos(getTodoComments()));
+            break;
+        case 'user':
+            printTodos(getTodosByUsername(args.toLowerCase(), getTodoComments()));
+            break;
+        case 'sort':
+            printTodos(sortTodos(args, getTodoComments()))
+            break;
+        case 'date':
+            printTodos(sortTodos(args, getTodoComments()))
             break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function splitFileToStrings(allFiles) {
+    let allStrings = [];
+    for (const file of allFiles) {
+        allStrings = allStrings.concat(file.split('\r\n'));
+    }
+    return allStrings;
+}
+
+function getTodoComments() {
+    let result = [];
+    let allStrings = splitFileToStrings(files);
+    for(let str of allStrings) {
+        let arr = str.split('\/\/ TODO ');
+        if (arr.length > 1) {
+            result = result.concat(arr[1]);
+        }
+    }
+    return getTodosObjectsFromArray(result);
+}
+
+function getTodosObjectsFromArray(todos){
+    let todosObj = [];
+    for (todo of todos) {
+        let exclamantionCount = todo.split('!').length - 1;
+        let newObj = { importance: exclamantionCount };
+
+        let todoElements = todo.split(';');
+        if (todoElements.length === 1){
+            newObj.user = '';
+            newObj.date = '';
+            newObj.comment = todoElements[0];
+        }
+        else {
+            newObj.user = todoElements[0];
+            newObj.date = todoElements[1].trim();
+            newObj.comment = todoElements[2].trim();
+        }
+        todosObj = todosObj.concat(newObj);
+    }
+    return todosObj;
+}
+
+function getImportantTodos(todos) {
+    return todos.filter(x => x.importance > 0);
+}
+
+function getTodosByUsername(username, todos){
+    return todos.filter(x => x.user.toLowerCase() === username);
+}
+
+function sortTodos(criteria, todos) {  
+    switch (criteria) {
+        case 'importance':
+            return sortImportance(todos);
+        case 'user':
+            return sortUsers(todos);
+        case 'date':
+            return sortDates(todos);    
+    }
+}
+
+function sortImportance(todos){
+    todos.sort(function (a, b) {
+        if (a.importance < b.importance) {
+          return 1;
+        }
+        if (a.importance > b.importance) {
+          return -1;
+        }        
+        return 0;
+      });
+    return todos;
+}
+
+function sortUsers(todos){
+    todos.sort(function (a, b) {       
+        if (a.user === '') {
+            return 1;
+        }
+        if (b.user === '') {
+            return -1;
+        }
+        if (a.user.toLowerCase() > b.user.toLowerCase()) {
+          return 1;
+        }
+        if (a.user.toLowerCase() < b.user.toLowerCase()) {
+          return -1;
+        }        
+        return 0;
+      });
+    return todos;
+}
+
+function sortDates(todos){
+    todos.sort(function (a, b) {       
+        if (a.date < b.date) {
+          return 1;
+        }
+        if (a.date > b.date) {
+          return -1;
+        }        
+        return 0;
+      });
+    return todos;
+}
+
+function printTodos(todos) {
+    let maxLenghtImportance = 1;
+    let maxLenghtUser = 0;
+    let maxLenghtDate = 0;
+    let maxLenghtComment = 0;
+    for (let todo of todos) {
+        if(todo.user.length > maxLenghtUser) {
+            maxLenghtUser = todo.user.length <= 10 ? todo.user.length : 10;
+        }
+        if(todo.date.length > maxLenghtDate) {
+            maxLenghtDate = todo.user.date <= 10 ? todo.date.length : 10;
+        }
+        if(todo.comment.length > maxLenghtComment) {
+            maxLenghtComment = todo.comment.length <= 50 ? todo.comment.length : 50;
+        }
+    }
+    
+    let sumLenght = maxLenghtImportance + maxLenghtDate + maxLenghtUser + maxLenghtComment + 3*1 + 7*2; // последнее символы | и пробелы
+    printRow({
+        importance: 1,
+        user: 'user',
+        date: 'date',
+        comment: 'comment'
+    }, maxLenghtUser , maxLenghtDate, maxLenghtComment)
+    console.log('-'.repeat(sumLenght))
+    for (let todo of todos) {
+        printRow(todo, maxLenghtUser , maxLenghtDate, maxLenghtComment)
+    }
+    console.log('-'.repeat(sumLenght))
+}
+
+function printRow(row, maxLenghtUser, maxLenghtDate, maxLenghtComment) {
+    let str = '  ' + (row.importance > 0 ? '!' : ' ').padEnd(1) + '  |  ';
+        str += (row.user.lenght <= maxLenghtUser ? row.user : row.user.slice(0, maxLenghtUser)).padEnd(maxLenghtUser);
+        str += '  |  '+ row.date.padEnd(maxLenghtDate) + '  |  ';
+        str += (row.comment.lenght <= maxLenghtComment ? row.comment : row.comment.slice(0, maxLenghtComment)).padEnd(maxLenghtComment);
+        console.log(str);
 }
 
 // TODO you can do it!
